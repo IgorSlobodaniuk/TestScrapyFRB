1424007ba9e462e1cefa804327f85c460dc51646
IgorSlobodaniuk <islobodanyuk@lvivsoft.com>
2016-12-21 22:48:30 +0200

    some fixes

--- FRBTestScraper/FRBTestScraper/spiders/frb.py
+++ FRBTestScraper/FRBTestScraper/spiders/frb.py
@@ -9,6 +9,7 @@ import scrapy
 
 BASE_URL = 'https://www.federalreserve.gov/releases/h10/hist/'
 TABLE_XPATH = './/table[@class="statistics"]/tr'
+C_XPATH = './th/a[contains(@href, "dat")]/{}'
 F_URI = '../results/scraped_result_{}.{}'
 FEED_FORMAT = 'csv'
 
@@ -20,24 +21,26 @@ class FrbSpider(scrapy.Spider):
 
     custom_settings = {
         'FEED_FORMAT': FEED_FORMAT,
-        'FEED_URI': F_URI.format(strftime("%Y-%m-%d %H:%M:%S", gmtime()), FEED_FORMAT.lower()),
+        'FEED_URI': F_URI.format(
+            strftime("%Y-%m-%d %H:%M:%S", gmtime()), FEED_FORMAT.lower()),
     }
 
     def parse(self, response):
-        """ Parsing table with countries
-
+        """
+        Parsing table with countries
         @url https://www.federalreserve.gov/releases/h10/hist/
-
         """
 
         for country in response.xpath(TABLE_XPATH):
-            country_name = country.xpath('.//a/text()').extract_first().strip()
-            url = BASE_URL + country.xpath('.//a/@href').extract_first()
+            country_name = country.xpath(C_XPATH.format('text()')).extract_first().strip()
+            url = BASE_URL + country.xpath(C_XPATH.format('@href')).extract_first()
             request = scrapy.Request(url, callback=self._parse_rates)
             request.meta['country'] = country_name
+
             yield request
 
-    def _parse_rates(self, response):
+    @staticmethod
+    def _parse_rates(response):
         """
         Parsing table with date and rate for a country
         @url https://www.federalreserve.gov/releases/h10/hist/
