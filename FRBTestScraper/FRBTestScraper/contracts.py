3a98ae198288fa65f240e8ab24b34979b42d2f29
IgorSlobodaniuk <igor.slobodaniuk@gmail.com>
2017-01-20 11:58:25 +0200

    Update contracts.py

--- FRBTestScraper/FRBTestScraper/contracts.py
+++ FRBTestScraper/FRBTestScraper/contracts.py
@@ -8,11 +8,8 @@ class HasProperTitleContract(Contract):
 
     name = 'is_in_title'
 
-    print 7777777777777777777777777777777777777777
-
     def pre_process(self, response):
         title = response.xpath('.//title/text()').extract_first()
         for i in self.args:
-            print 56565656565656565
             if i not in title:
                 raise ContractFail('Incorrect title')
